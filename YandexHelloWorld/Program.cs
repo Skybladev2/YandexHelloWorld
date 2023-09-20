33f0a11e777991474e4d657d01c77966b4d9ab7e
Skybladev2 <Skybladev2@users.noreply.github.com>
2023-09-20 17:24:11 +0300

    working example

--- YandexHelloWorld/Program.cs
+++ YandexHelloWorld/Program.cs
@@ -7,18 +7,14 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
+        builder.WebHost.UseUrls("http://127.0.0.1:" + Environment.GetEnvironmentVariable("PORT"));
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-
         app.UseAuthorization();
-
-
         app.MapControllers();
-
         app.Run();
     }
 }
