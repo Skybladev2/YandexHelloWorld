5b0fb1a28d99fc0d882802c79bca7c254e1dbc91
Skybladev2 <Skybladev2@users.noreply.github.com>
2023-09-20 14:57:28 +0300

    firat commit

---
+++ YandexHelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YandexHelloWorld.Controllers;
+
+[ApiController]
+[Route("")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello";
+    }
+}
+
